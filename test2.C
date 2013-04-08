af87c69a35dab37475505e6b33c4c3377c1311d8
Roy H. Stogner <roystgnr@ices.utexas.edu>
2013-04-08 15:08:10 -0500

    Fix the merged program

--- test2.C
+++ test2.C
@@ -6,13 +6,13 @@
 int test2func1(void)
 {
   std::cout << "Test 2 func 1" << std::endl;
-  testfunc1();
-  return testfunc1();
+  test_func1();
+  return test_func1();
 }
 
 int test2func2(void)
 {
   std::cout << "Test 2 func 2" << std::endl;
-  testfunc2();
-  return testfunc2();
+  test_func2();
+  return test_func2();
 }
