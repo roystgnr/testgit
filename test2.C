20b37730c955a08fb6068a42d710f7de66544757
Roy H. Stogner <roystgnr@ices.utexas.edu>
2013-04-08 15:06:17 -0500

    Second set of changes to branch

--- test2.C
+++ test2.C
@@ -6,11 +6,13 @@
 int test2func1(void)
 {
   std::cout << "Test 2 func 1" << std::endl;
+  testfunc1();
   return testfunc1();
 }
 
 int test2func2(void)
 {
   std::cout << "Test 2 func 2" << std::endl;
+  testfunc2();
   return testfunc2();
 }
